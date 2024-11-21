4ba885ea756213b85b7b5806e994acf959585a73
Hz1009607 <zhe_huang@mymail.sutd.edu.sg>
2024-11-21 12:43:14 +0800

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,4 +1,4 @@
-const margin = { top: 50, right: 30, bottom: 50, left: 60 };
+const margin = { top: 50, right: 150, bottom: 50, left: 60 };
 const width = 800 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
@@ -15,30 +15,63 @@ d3.csv("data.csv").then(data => {
         d.resale_price = +d.resale_price;
     });
 
-    const xScale = d3.scaleBand()
-        .domain(data.map(d => d.year))
-        .range([0, width])
-        .padding(0.1);
+    const nestedData = d3.group(data, d => d.flat_type);
+
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.year))
+        .range([0, width]);
 
     const yScale = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.resale_price)])
         .range([height, 0]);
 
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
     svg.append("g")
         .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(xScale));
+        .call(d3.axisBottom(xScale).tickFormat(d3.format("d")))
+        .append("text")
+        .attr("x", width / 2)
+        .attr("y", 40)
+        .attr("fill", "black")
+        .attr("class", "axis-label")
+        .text("Year");
 
     svg.append("g")
-        .call(d3.axisLeft(yScale));
+        .call(d3.axisLeft(yScale))
+        .append("text")
+        .attr("x", -height / 2)
+        .attr("y", -40)
+        .attr("transform", "rotate(-90)")
+        .attr("fill", "black")
+        .attr("class", "axis-label")
+        .text("Average Price (SGD)");
 
-    const line = d3.line()
-        .x(d => xScale(d.year) + xScale.bandwidth() / 2)
-        .y(d => yScale(d.resale_price));
+    nestedData.forEach((values, key) => {
+        svg.append("path")
+            .datum(values)
+            .attr("class", "line")
+            .attr("fill", "none")
+            .attr("stroke", colorScale(key))
+            .attr("d", d3.line()
+                .x(d => xScale(d.year))
+                .y(d => yScale(d.resale_price))
+            );
+    });
 
-    svg.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 2)
-        .attr("d", line);
+    const legend = svg.selectAll(".legend")
+        .data(nestedData.keys())
+        .enter().append("g")
+        .attr("transform", (d, i) => `translate(${width + 10}, ${i * 20})`);
+
+    legend.append("rect")
+        .attr("width", 15)
+        .attr("height", 15)
+        .attr("fill", d => colorScale(d));
+
+    legend.append("text")
+        .attr("x", 20)
+        .attr("y", 12)
+        .attr("class", "legend")
+        .text(d => d);
 });
